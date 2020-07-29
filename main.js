4c82e77090f65ef7572c127d7e00866e76d3e85e
russellcxl <russellcxl@gmail.com>
2020-07-29 21:47:30 +0800

    sort button added, functional, shows only end result

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ let bars = []
 let numOfBars = 70;
 let $container = document.querySelector(".main-container");
 let $randomiser = document.querySelector(".button-randomise");
-let $sorter = document.querySelector("button-sort");
+let $sorter = document.querySelector(".button-sort");
 
 for (let i = 0; i < numOfBars; i++) {
     let $newBar = document.createElement("div");
     $newBar.style.height = `${Math.random() * 90}%`;
     $newBar.className = "bar mr-1";
-    bars.push($newBar);
+    bars.push(parseFloat($newBar.style.height));
     $container.append($newBar);
 }
 
@@ -22,18 +22,24 @@ $randomiser.addEventListener("click", function() {
         let $newBar = document.createElement("div");
         $newBar.style.height = `${Math.random() * 90}%`;
         $newBar.className = "bar mr-1";
-        bars.push($newBar);
+        bars.push(parseFloat($newBar.style.height));
         $container.append($newBar);
     }
 });
 
-
+$sorter.addEventListener("click", function() {
+    quickSort(bars);
+    let $bars = document.getElementsByClassName("bar");
+    for (let i = 0; i < $bars.length; i++) {
+        $bars[i].style.height = `${bars[i]}%`;        
+    }
+});
 
 //==================== quick sort O(N^2) ====================//
 
 //taking pivot as last element
 //done when start index = end index i.e. no array left to sort
-function quickSort(arr, start, end) {
+function quickSort(arr, start = 0 , end = arr.length - 1) {
     if (start >= end) return;
     
     let index = partition(arr, start, end);
@@ -61,4 +67,7 @@ function swap(arr, i, j) {
     arr.splice(i, 0, extracted[0]);
 }
 
-let arr= [2, 23, 56, 21, 6, 10];
+//==================== visualisation ====================//
+
+
+
